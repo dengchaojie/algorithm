adbbbb2de2e0b6d610ed1b56f1ca17b5337c564a
dengchaojie <dengchaojiedcj@163.com>
2019-08-26 21:32:56 +0800

    108

--- 108.playground/Contents.swift
+++ 108.playground/Contents.swift
@@ -27,54 +27,41 @@ class Solution {
             return TreeNode.init(nums[0])
         }
         if count == 2 {
+            let node = TreeNode.init(nums[0])
+            node.left = nil
+            node.right = TreeNode.init(nums[1])
+            return node
+        }
+        if count == 3 {
             let node = TreeNode.init(nums[1])
             node.left = TreeNode.init(nums[0])
+            node.right = TreeNode.init(nums[2])
             return node
         }
-
-        let index = count / 2
-        let c = nums[index]
+        let index = count / 2 + count % 2
+        let c = nums[index-1]
         
         let node = TreeNode.init(c)
-        node.left = TreeNode.init(index-1)
-        node.right = TreeNode.init(index+1)
-
-        let leftA = Array(nums[0...(index-1)])
-        node.left!.left = self.sortedArrayToLeftBST(leftA)
+        let leftA = Array(nums[0...(index-2)])
+        node.left = self.sortedArrayToBST(leftA)
         
-        let rightA = Array(nums[(index+1)...(count-1)])
-        node.right!.left = self.sortedArrayToLeftBST(rightA)
-        return node
-    }
-    
-    func sortedArrayToLeftBST(_ nums: [Int]) -> TreeNode? {
-        let count = nums.count
-        if count == 0 {
-            return nil
-        }
-        if count == 1 {
-            return TreeNode.init(nums[0])
-        }
-        if count == 2 {
-            let node = TreeNode.init(1)
-            node.left = TreeNode.init(0)
-            return node
-        }
+        let rightA = Array(nums[(index)...(count-1)])
+        node.right = self.sortedArrayToBST(rightA)
         
-        let node = TreeNode.init(count - 1)
-        node.left = self.sortedArrayToLeftBST(nums)
 
         return node
     }
-    
-    
 }
 
 
 let obj = Solution.init()
 //print(obj.sortedArrayToBST([]) ?? "default value0")
 //print(obj.sortedArrayToBST([-10]) ?? "default value1")
-print(obj.sortedArrayToBST([-10,-3]) ?? "default value2")
+//print(obj.sortedArrayToBST([-10,-3]) ?? "default value2")
 //print(obj.sortedArrayToBST([-10,-3,0]) ?? "default value3")
 //print(obj.sortedArrayToBST([-10,-3,0,5]) ?? "default value4")
-//print(obj.sortedArrayToBST([-10,-3,0,5,9]) ?? "default value5")
+print(obj.sortedArrayToBST([-10,-3,0,5,9]) ?? "default value5")
+//print(obj.sortedArrayToBST([-10,-3,0,5,9, 10]) ?? "default value5")
+
+//print(obj.sortedArrayToBST([-10,-3,0,5,9,10, 11]) ?? "default value5")
+
