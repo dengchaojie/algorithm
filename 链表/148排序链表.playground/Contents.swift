479e7c5b910076d7b00de590476226535673b665
dengchaojie <dengchaojiedcj@163.com>
2019-09-12 08:01:35 +0800

    148

--- 链表/148排序链表.playground/Contents.swift
+++ 链表/148排序链表.playground/Contents.swift
@@ -13,55 +13,52 @@ public class ListNode {
 
 class Solution {
     func sortList(_ head: ListNode?) -> ListNode? {
-        
-    }
-}
-/*
-class Solution {
-    func sortList(_ head: ListNode?) -> ListNode? {
         let dummy = ListNode(-1)
-        var t1 = ListNode(-1)
+
         var temp = head
-        print(head?.val as Any)
-        while temp != nil {
+        var tempHead = head?.next
+//        print(head?.val as Any)
+        while temp != nil
+        {
             var node = dummy
             var temp2 = dummy.next
-            while temp2 != nil {
-                if temp!.val < temp2!.val {
-                    temp!.next = temp2
-                    node.next = temp
-                    
+            while temp2 != nil
+            {
+                if temp!.val <= temp2!.val {
+
                     break
                 }else {
-                    print(temp2!.val)
+//                    print(temp2!.val)
                     node = temp2!
                     temp2 = temp2!.next
                 }
             }
-            if temp2 == nil {
-                node.next = temp
-
-            }else {
-                
-            }
-            t1 = temp!
-            temp = temp!.next
-            print(temp?.val as Any)
+            node.next = temp
+            tempHead = temp!.next
+            temp?.next = temp2
+            temp = tempHead
+//            print(temp?.val as Any)
+        
         }
         return dummy.next
     }
 }
-*/
+
 let l0 = ListNode(3)
 let l1 = ListNode(1)
 let l2 = ListNode(2)
+let l3 = ListNode(4)
+let l4 = ListNode(0)
+
 l0.next = l1
 l1.next = l2
+l2.next = l3
+l3.next = l4
 let obj = Solution()
 let res = obj.sortList(l0)
 var temp = res
-//while let t = temp {
-//    print(t.val)
-//    temp = t.next
-//}
+while let t = temp {
+    print(t.val)
+    temp = t.next
+}
 
