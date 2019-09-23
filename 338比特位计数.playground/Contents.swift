52b74362b8381c8f3e10b29fcb8c3c56b1cde39b
dengchaojie <dengchaojiedcj@163.com>
2019-09-23 08:30:49 +0800

    78

---
+++ 338比特位计数.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+//https://leetcode-cn.com/problems/counting-bits/
+class Solution {
+    func countBits(_ num: Int) -> [Int] {
+        
+        var index = 0
+        var resArr = [Int]()
+        var beforeCount = 0
+        while index <= num {
+            
+        }
+        return resArr
+    }
+    
+    
+    
+}
+
---
+++ 78子集.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+//https://leetcode-cn.com/problems/subsets/
+class Solution {
+    func subsets(_ nums: [Int]) -> [[Int]] {
+        
+        
+        return [[]]
+    }
+}
