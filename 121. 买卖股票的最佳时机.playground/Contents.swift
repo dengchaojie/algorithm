9a5b03980dca2bf58d001b56bbffd3420b47e550
dengchaojie <dengchaojiedcj@163.com>
2019-10-19 10:52:17 +0800

    new

--- 121. 买卖股票的最佳时机.playground/Contents.swift
+++ 121. 买卖股票的最佳时机.playground/Contents.swift
@@ -3,24 +3,42 @@ import UIKit
 //https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
 // si lu: ji suan yi ge zui da cha zhi
 // dong tai gui hua wen ti [7,1,5,3,6,4]
+// yi ci bian li，看的题解，我没有做出来。我花了100分钟
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var minPrice = Int.max
+        var maxProfit = 0
+        for item in prices {
+            if item < minPrice {
+                minPrice = item
+            }else if (item - minPrice) > maxProfit {
+                maxProfit = item - minPrice
+            }
+        }
+        return maxProfit
+    }
+}
+let obj = Solution()
+print(obj.maxProfit([7,1,5,3,6,4]))
+/// zuo pai xu
+// bao li bian li
+
+
+
 //class Solution {
 //    func maxProfit(_ prices: [Int]) -> Int {
 //        var sub = 0
 //        let cou = prices.count
-//        var dict = [Int: Int]()
-//        var min = prices.first!
-//        var max = min
-//        dict[min] = 0
-//        dict[max] = 0
+//        let sortedPri = prices.sorted()
+//        let min = sortedPri.first!
+//        let max = sortedPri.last!
+//        var minIndex = -1
+//        var maxIndex = -1
 //        for (n, item) in prices.enumerated() {
-//            if item < min {
-//                let maxIndex = dict[max]!
-//                if n < cou - 1 {
-//                    min = item
-//                    dict[min] = min
-//                }
-//            }else if item >= max {
-//                let minIndex = dict[min]
+//            if item == min {
+//                minIndex = n
+//            }else if item == max {
+//                maxIndex = n
 //            }
 //        }
 //        return sub
@@ -28,62 +46,62 @@ import UIKit
 //}
 /// 最后一个不能是最小值，第一个不能是最大值
 /// 最小值最好靠前，最的值最好靠后
-class Solution {
-    func maxProfit(_ prices: [Int]) -> Int {
-        var sub = 0
-
-        let cou = prices.count
-        if cou == 0 {
-            return 0
-        }
-        if cou == 1 {
-            return 0
-        }
-        if cou == 2 {
-            let sub = prices[1] - prices [0]
-            if sub > 0 {
-                return sub
-            }else {
-                return 0
-            }
-        }
-        let sortA = prices.sorted()
-        let min = sortA.first!
-        let max = sortA.last!
-        var dict = [Int:Int]()
-        for (n, item) in prices.enumerated() {
-            if dict[item] != nil {
-                if item == min {
-
-                }else if item == max {
-                    dict[item] = n
-                }
-            }else {
-                if item == min && n == (cou - 1) {
-                    
-                }else if item == max && n == 0 {
-                    
-                }else {
-                    dict[item] = n
-                }
-            }
-        }
-        let minIndex = dict[min]
-        let maxIndex = dict[max]
-        if minIndex! < maxIndex! {
-            sub = max - min
-        }else {
-            let keysSorted = dict.keys.sorted()
-//            for item in keysSorted {
+//class Solution {
+//    func maxProfit(_ prices: [Int]) -> Int {
+//        var sub = 0
 //
+//        let cou = prices.count
+//        if cou == 0 {
+//            return 0
+//        }
+//        if cou == 1 {
+//            return 0
+//        }
+//        if cou == 2 {
+//            let sub = prices[1] - prices [0]
+//            if sub > 0 {
+//                return sub
+//            }else {
+//                return 0
 //            }
-            
-        }
-
-        return sub
-    }
-
-}
+//        }
+//        let sortA = prices.sorted()
+//        let min = sortA.first!
+//        let max = sortA.last!
+//        var dict = [Int:Int]()
+//        for (n, item) in prices.enumerated() {
+//            if dict[item] != nil {
+//                if item == min {
+//
+//                }else if item == max {
+//                    dict[item] = n
+//                }
+//            }else {
+//                if item == min && n == (cou - 1) {
+//
+//                }else if item == max && n == 0 {
+//
+//                }else {
+//                    dict[item] = n
+//                }
+//            }
+//        }
+//        let minIndex = dict[min]
+//        let maxIndex = dict[max]
+//        if minIndex! < maxIndex! {
+//            sub = max - min
+//        }else {
+//            let keysSorted = dict.keys.sorted()
+//            for item in keysSorted {
+//                if
+//            }
+//
+//        }
+//
+//        return sub
+//    }
+//
+//}
 //class Solution {
 //    func maxProfit(_ prices: [Int]) -> Int {
 //        let cou = prices.count
@@ -127,8 +145,6 @@ class Solution {
 //    }
 //}
 
-let obj = Solution()
-print(obj.maxProfit([7,1,5,3,6,4]))
-/// zuo pai xu
+
 
 
--- 122. 买卖股票的最佳时机 II.playground/Contents.swift
+++ 122. 买卖股票的最佳时机 II.playground/Contents.swift
@@ -1,14 +1,94 @@
 import UIKit
 
 //https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+///这道题，我看了题解
+/// 方法1，峰谷法
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        var sum = 0
+        if prices.count == 0 {
+            return 0
+
+        }
+        var maxProfit = 0
+        var gu = prices[0]
+        var feng = gu
+        var i = 0
         
+        while i < prices.count - 1 {
+            while i < prices.count - 1 && prices[i] >= prices[i+1] {
+                i += 1
+            }
+            gu = prices[i]
+            while i < prices.count - 1 && prices[i] <= prices[i+1]   {
+                i += 1
+            }
+            feng = prices[i]
+            maxProfit += feng - gu
+        }
         
-        
-        return sum
+        return maxProfit
     }
+    
 }
+/// 方法2，简单的一次遍历：将数字连线，将先划分了一段段由最小值到最大值的连线，求每小段的最大利润，在求和
+//class Solution {
+//    func maxProfit(_ prices: [Int]) -> Int {
+//        if prices.count == 0 {
+//            return 0
+//
+//        }
+//        var maxProfit = 0
+//        for i in 1..<prices.count {
+//            if prices[i] > prices[i-1] {
+//                maxProfit += prices[i] - prices[i-1]
+//            }
+//        }
+//
+//        return maxProfit
+//    }
+//
+//}
+//class Solution {
+//    func maxProfit(_ prices: [Int]) -> Int {
+//        var sumProfit = 0
+//
+//        var sum = 0
+//        var pingJunZhi = 0.0
+//        for item in prices {
+//            sum += item
+//
+//        }
+//        pingJunZhi = Double(sum) / Double(prices.count)
+//        var maiRu = -1
+//        var maiChu = -1
+//        for item in prices {
+//            if Double(item) < pingJunZhi {
+//                if maiRu > 0 && item > maiRu {
+//
+//                }else
+//                {
+//                    maiRu = item
+//                }
+//            }else {
+//                if maiRu > 0 {
+//                    maiChu = item
+//                    sumProfit += maiChu - maiRu
+//                    maiRu = -1
+//                    maiChu = -1
+//                }
+//            }
+//        }
+//        return sumProfit
+//    }
+//}
+
+//[7,1,5,3,6,4] 求最优解，
+// 平均值是26/6 = 4.33
+// 最小值是1，最大值是7
+
+let obj = Solution()
+//print(obj.maxProfit([7,1,5,3,6,4]))
+print(obj.maxProfit([1,2,3,4,5]))
+//print(obj.maxProfit([7,6,4,3,1]))
 
 
