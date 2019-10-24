84e4f49b5d4dd27ef2fe0be7cbd8423e49a38fd3
dengchaojie <dengchaojiedcj@163.com>
2019-10-25 07:59:47 +0800

    new

--- 59. 螺旋矩阵 II.playground/Contents.swift
+++ 59. 螺旋矩阵 II.playground/Contents.swift
@@ -7,7 +7,11 @@ class Solution {
     func generateMatrix(_ n: Int) -> [[Int]] {
         var arrYiWei = Array.init(repeating: 0, count: n)
         var arr = Array.init(repeating: arrYiWei, count: n)
-        
+        let target = n*n
+        var l = 0, r = 0, t = 0, b = 0
+        for i in 1...target {
+            
+        }
         return arr
         
     }
