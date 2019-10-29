834c08f7c162a98dcd5c4b8b2d32d8c7d7380707
dengchaojie <dengchaojiedcj@163.com>
2019-10-29 08:14:34 +0800

    new

--- 78子集.playground/Contents.swift
+++ 78子集.playground/Contents.swift
@@ -1,11 +1,51 @@
 
 //https://leetcode-cn.com/problems/subsets/
+
 class Solution {
-    func subsets(_ nums: [Int]) -> [[Int]] {
+    func subsets(nums: [Int]) -> [[Int]] {
+        var res = [[Int]]()
+        var path = [Int]()
         
+        let nums = nums.sorted(by: <)
         
-        return [[]]
+        _dfs(res: &res, &path, nums, 0)
+        
+        return res
+    }
+    
+    private func _dfs(res: inout [[Int]],  _ path: inout [Int], _ nums: [Int], _ index: Int) {
+        // termination case
+
+        res.append(Array(path))
+        print(res)
+        for i in index..<nums.count {
+            print(i)
+            path.append(nums[i])
+            print(path)
+            _dfs(res: &res, &path, nums, i + 1)
+            let re = path.removeLast()
+            print(re)
+            
+        }
     }
 }
 
+let obj = Solution()
+obj.subsets(nums: [1,2,3])
 
+//class Solution {
+//    func subsets(_ nums: [Int]) -> [[Int]] {
+//
+//
+//        return [[]]
+//    }
+//}
+
+
+//1 2 3
+//
+//path    res            index
+//[]      []            0
+//1        1            1
+//1 2     1 2
+//1 2 3   1 2 3
