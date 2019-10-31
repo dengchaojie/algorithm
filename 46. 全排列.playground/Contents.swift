98640dccfbfcba17752dc29e010cee2c2f092b9e
dengchaojie <dengchaojiedcj@163.com>
2019-10-31 13:34:05 +0800

    new

---
+++ 46. 全排列.playground/Contents.swift
@@ -0,0 +1,10 @@
+//https://leetcode-cn.com/problems/permutations/
+// silu
+
+class Solution {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        var res = [[Int]]()
+        
+        return res
+    }
+}
---
+++ 62. 不同路径.playground/Contents.swift
@@ -0,0 +1,11 @@
+//https://leetcode-cn.com/problems/unique-paths/
+
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        var res = 0
+        
+        
+        
+        return res
+    }
+}
