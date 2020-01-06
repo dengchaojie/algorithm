ed6aab84996d468b29caaa21106121adee158307
dengchaojie <dengchaojiedcj@163.com>
2020-01-06 21:54:06 +0800

    new

---
+++ 75. 颜色分类.playground/Contents.swift
@@ -0,0 +1,7 @@
+
+//https://leetcode-cn.com/problems/sort-colors/
+class Solution {
+    func sortColors(_ nums: inout [Int]) {
+        
+    }
+}
---
+++ 链表/146. LRU缓存机制.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//https://leetcode-cn.com/problems/lru-cache/?utm_source=LCUS&utm_medium=ip_redirect_q_uns&utm_campaign=transfer2china
+
+
+class LRUCache {
+
+    init(_ capacity: Int) {
+        
+    }
+    
+    func get(_ key: Int) -> Int {
+        
+    }
+    
+    func put(_ key: Int, _ value: Int) {
+        
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * let obj = LRUCache(capacity)
+ * let ret_1: Int = obj.get(key)
+ * obj.put(key, value)
+ */
