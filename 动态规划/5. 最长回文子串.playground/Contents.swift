44b7d21d4dad812924a4e6ed6a243168087da87a
dengchaojie <dengchaojiedcj@163.com>
2020-01-20 20:52:42 +0800

    new

---
+++ 动态规划/5. 最长回文子串.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+//https://leetcode-cn.com/problems/longest-palindromic-substring/
+/*
+ 42
+ 
+ */
+class Solution {
+    func longestPalindrome(_ s: String) -> String {
+        var max_len = 0
+        var charDict = [Character: Int]()
+        let chars = Array(s)
+        for (index, item) in chars.enumerated() {
+            
+        }
+        
+        
+    }
+}
