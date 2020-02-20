ceeddf9b432ccbb6da47eef05d9a1d2cb59e1f61
dengchaojie <dengchaojiedcj@163.com>
2020-02-20 23:09:19 +0800

    new

--- 面经/优先级队列用最小堆实现.playground/Contents.swift
+++ 面经/优先级队列用最小堆实现.playground/Contents.swift
@@ -10,20 +10,58 @@
  [1,2,5,3,4,6,7]
  */
 class Solution {
-    var minHeap: [Int]
+//    var minHeap: [Int]
+//
+//    init(minHeap:[Int]) {
+//        self.minHeap = minHeap
+//    }
+//
+//    func insertValue(_ value: Int) {
+//
+//    }
+//
+//    func excuteValue(_ value: Int) {
+//
+//    }
+//
     
-    init(minHeap:[Int]) {
-        self.minHeap = minHeap
+    func isPalindrome(_ x: Int) -> Bool {
+        let str = String(x)
+        
+        if str.count == 1 {
+            return true
+        }
+        
+        let chars = Array(str)
+        var left = 0, right = str.count-1
+        while left < right {
+            if chars[left] == chars[right] {
+                left += 1
+                right -= 1
+            }else {
+                return false
+            }
+        }
+        return true
     }
     
-    func insertValue(_ value: Int) {
+    func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+        return 1
+    }
+    
+    func numSquares(_ n: Int) -> Int {
+        //
+        var resultArr = Array(repeating: 0, count: n+1)
+        resultArr[0] = 1
+        for i in 1...n {
+            
+        }
+        
         
     }
-    
-    func excuteValue(_ value: Int) {
-        
-    }
-    
     
 }
+
+let obj = Solution()
+obj.isPalindrome(101)
 
